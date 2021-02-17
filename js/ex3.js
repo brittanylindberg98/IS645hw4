392f3f06928a6fd8806bba2a7a5f818f5e18557f
Brittany Lindberg <blindberg29@gmail.com>
2021-02-16 17:56:58 -0800

    commit

--- js/ex3.js
+++ js/ex3.js
@@ -7,14 +7,8 @@ const values = [3, 11, 7, 2, 9, 10];
 
 let sum = 0;
 
-// Solution using a forEach method
-values.forEach(value => {
+for (const value of values) {
   sum += value;
-});
-
-// Alternative solution using a for-of loop
-/* for (const value of values) {
-  sum += value;
-} */
+} 
 
 console.log(sum);
